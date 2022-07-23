98013f0c10b3d1bd01186d0c799eeec8414d3180
Scott Chau <scottshing@yahoo.com.hk>
2022-07-23 14:26:52 -0400

    Basic function set up

--- script.js
+++ script.js
@@ -2,61 +2,98 @@
 function computerChoice() {
   randomNumber = Math.floor(Math.random() * 3);
   if (randomNumber == 0) {
-    return "Rock";
+    return "rock";
   } else if (randomNumber == 1) {
-    return "Paper";
+    return "paper";
   } else if (randomNumber == 2) {
-    return "Scissors";
+    return "scissors";
   }
 }
 
 let playerScore = 0;
 let computerScore = 0;
 
-function gameStart(playerSelection, computerSelection) {
-  let playerChoice = playerSelection.toLowerCase();
-  playerChoice = playerChoice[0].toUpperCase() + playerChoice.slice(1);
-  console.log(`Player:${playerChoice}, Computer:${computerSelection}`);
+function score() {
+  const container = document.querySelector("#container");
+  const content = document.createElement("div");
+  content.classList.add("content");
+  content.textContent = `Player Score: ${playerScore} Computer Score ${computerScore}`;
+  container.appendChild(content);
+}
 
-  // Player wins
-  if (playerChoice === computerSelection) {
-    console.log("Draw!");
-  } else if (
-    (computerSelection == "Rock" && playerChoice == "Paper") ||
-    (computerSelection == "Paper" && playerChoice == "Scissors") ||
-    (computerSelection == "Scissors" && playerChoice == "Rock")
-  ) {
-    console.log(`You win! ${playerChoice} beats ${computerSelection}`);
-    return (playerScore += 1);
-
-    // Computer wins
-  } else if (
-    (playerChoice == "Rock" && computerSelection == "Paper") ||
-    (playerChoice == "Paper" && computerSelection == "Scissors") ||
-    (playerChoice == "Scissors" && computerSelection == "Rock")
-  ) {
-    console.log(`You lose! ${computerSelection} beats ${playerChoice}`);
-    return (computerScore += 1);
-    // Invaild input
-  } else {
-    console.log(`Invalid Input, please type "Rock, Paper,Scissor" `);
+function rockC() {
+  computer = computerChoice();
+  console.log(`Computer:${computer}`);
+  if (computer == "rock") {
+    return score();
+  } else if (computer == "paper") {
+    console.log("You lose!");
+    computerScore += 1;
+    return score();
+  } else if (computer == "scissors") {
+    console.log("You win");
+    playerScore += 1;
+    return score();
   }
 }
 
-function game() {
-  for (let i = 0; i < 5; i++) {
-    gameStart(prompt("Rock, Paper or Scissors?"), computerChoice());
-    console.log(
-      `Player Scores: ${playerScore} Computer Scores ${computerScore}`
-    );
-  }
-  if (playerScore > computerScore) {
-    console.log("Player wins");
-  } else if (computerScore > playerScore) {
-    console.log("Computer wins");
-  } else {
-    console.log("Draw");
+function paperC() {
+  computer = computerChoice();
+  console.log(`Computer:${computer}`);
+  if (computer == "paper") {
+    return score();
+  } else if (computer == "scissors") {
+    console.log("You lose");
+    computerScore += 1;
+    return score();
+  } else if (computer == "rock") {
+    console.log("You win");
+    playerScore += 1;
+    return score();
   }
 }
 
-game();
+function scissorsC() {
+  computer = computerChoice();
+  console.log(`Computer:${computer}`);
+  if (computer == "scissors") {
+    return score();
+  } else if (computer == "rock") {
+    console.log("You lose");
+    computerScore += 1;
+    return score();
+  } else if (computer == "paper") {
+    console.log("You win");
+    playerScore += 1;
+    return score();
+  }
+}
+
+const rock = document.querySelector("#rock");
+rock.addEventListener("click", rockC);
+
+const paper = document.querySelector("#paper");
+paper.addEventListener("click", paperC);
+
+const scissors = document.querySelector("#scissors");
+scissors.addEventListener("click", scissorsC);
+
+// adding content
+
+// function game() {
+//   for (let i = 0; i < 5; i++) {
+//     gameStart(prompt("Rock, Paper or Scissors?"), computerChoice());
+//     console.log(
+//       `Player Scores: ${playerScore} Computer Scores ${computerScore}`
+//     );
+//   }
+//   if (playerScore > computerScore) {
+//     console.log("Player wins");
+//   } else if (computerScore > playerScore) {
+//     console.log("Computer wins");
+//   } else {
+//     console.log("Draw");
+//   }
+// }
+
+// game();
